23f8d954a73e34f74ed3f3e8214f1f1a2276a304
CoumingX <52475322+CoumingX@users.noreply.github.com>
2019-07-06 23:50:04 -0700

    Create InspecTor.sh

---
+++ InspecTor.sh
@@ -0,0 +1,115 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Tools-Th3InspecTor" | lolcat
+echo "\033[35;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mAuthor : CoumingX" 
+echo "\033[35;1mTeam : Anonymous"
+echo "\033[33;1mFacebook : Oh
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mPilih Sendiri Ya Ngentod !:)?:"
+echo "\033[36;1m"
+echo "[===============================================]>"
+echo "1.> Virus VbugMaker Creator"
+echo "[===============================================]>"
+echo "2.> Katoolin"
+echo "[===============================================]>"
+echo "3.> MBF Buat Hack Facebook"
+echo "[===============================================]>"
+echo "4. Install Santet-Online"
+echo "[===============================================]>"
+echo "5. Install RouterSploit"
+echo "[===============================================]>"
+echo -e "╭─Coumingx[PILIH NOMORNYA NGENTOD !]" | lolcat
+read -p "╰─#" Pilih NGENTOD!;
+
+if [ $vro = 1 ] || [ $vro = 1 ]
+then
+clear
+toilet "CoumingX"
+cd module
+python2 anvima.py
+fi
+
+if [ $vro = 2 ] || [ $vro = 2 ]
+then
+clear
+cowsay -f sheep.cow "CoumingX" | lolcat
+figlet "Tools-Th3InspecTor" | lolcat
+sleep 1
+cd
+cd module
+python2 katoolin.py
+fi
+
+if [ $vro = 3 ] || [ $vro = 3 ]
+then
+clear
+cowsay -f kiss.cow "CoumingX" | lolcat
+figlet "Tools-Th3InspecTor" | lolcat
+echo "\033[33;1muntuk tutorial kunjungi:" "\033[36;1m https://youtu.be/FsI4pCapfLY"
+sleep 3
+cd module
+python2 mbf.py
+fi
+
+if [ $vro = 4 ] || [ $vro = 4 ]
+then
+clear
+cowsay -f kiss.cow "CoumingX" | lolcat
+figlet "Tools-Th3InspecTor" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install php
+apt install git
+apt install python2
+git clone https://github.com/Gameye98/santet-online
+mv santet-online $HOME
+cd $HOME/santet-online
+pip2 install -r requirements.txt
+clear
+chmod +x santet.py
+python2 santet.py 
+fi
+
+if [ $vro = 4 ] || [ $vro = 4 ]
+then
+clear
+cowsay -f kiss.cow "CoumingX" | lolcat
+figlet "Tools-Th3InspecTor" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install python2
+https://github.com/reverse-shell/routersploit.git
+clear
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not ForGive"
+sleep 1
+echo "We Do Not ForGet"
+sleep 1
+echo "The Indonesian Anonymous Family"
+sleep 1
+echo "Expect Us"
+sleep 1
+exit
+fi
