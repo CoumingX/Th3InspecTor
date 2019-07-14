3de0271ba24737aff85248d056185c1269c8f8d0
CoumingX <52475322+CoumingX@users.noreply.github.com>
2019-07-14 14:14:09 +0700

    Update InspecTor.sh

--- InspecTor.sh
+++ InspecTor.sh
@@ -1,146 +1,79 @@
-#!/bin/bash
-#version 1.0
+#Cape Cok Buat Nya :)
 
-#MAU NGAPAIN TOT?:V
-#RECODE?REEDIT?RECOPYRIGHT?COPAS?
-#MALU TOLOL!!!
-#KAGA BERMORAL!
+import LIST
+from LIST.id import *
+from LIST.it import *
+from LIST.jp import *
+from LIST.us import *
+from LIST.fr import *
+from LIST.kr import *
+from LIST.de import *
+from LIST.tr import *
+import requests,re,os
 
-
-#08/07/19
-
-clear
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
-
-echo ""
-
-figlet Th3InspecTor  | lolcat
-
-echo -b "_____________________________________________________________" | lolcat
-echo -b "TYPE      : TOOLS Th3InsprcTor $green " |lolcat
-echo -b "VERSION   : V.1 " | lolcat
-echo -b "AUTHOR    : CoumingX $green " | lolcat
-echo -b "ASSOCIATE : Anonymous $green " |lolcat
-echo -b "NOTES     : JELAJAHI SETIAP TOOLS DENGAN TELITI DAN BIJAK" | lolcat
-echo -b "_____________________________________________________________" | lolcat
-
-sleep 1
-
-
-echo -b $green"[#]> TOOLS INSTALLER BY CoumingX " |lolcat
-echo -b $green"[#]> Thanks For Using My Tools" |lolcat
-sleep 1
-echo -b $green"[#]> CoumingX On Your System" |lolcat
-echo -b $green"[#]> See you Again Ngentod :)..." |lolcat
-figlet Th3InspecTor | lolcat
-sleep 1
-exit
-}
-
-
-lagi=1
-while [ $lagi -lt 5 ];
-do
-
-echo ""
-
-echo -e "######################################" | lolcat
-echo -e "#SILAHKAN TUNGGU YA NGRNTOD! #" | lolcat
-echo -e "######################################" | lolcat
-
-echo ""
-echo -e "============================" | lolcat
-echo -e $b "1. Install Drak-Fb${enda}";
-echo -e "============================" | lolcat
-echo -e $c "2. Install Hack Cctv${enda}";
-echo -e "============================" | lolcat
-echo -e $w "3. Install Nmap${enda}";
-echo -e "============================" | lolcat
-echo -e $g "4. Install Th3inspector${enda}";
-echo -e "============================" | lolcat
-echo -e $b "5. Install Hammer${enda}";
-echo -e "============================" | lolcat
-echo -e $b "00. Exit${enda}";
-
-echo ""
-
-#Install Dark-Fb
-
-1) git clone https://github.com/rezadkim/dark-fb
-echo -e "${y} Installer Dark-Fb..."
-echo -e "${y} cd dark-fb"
-echo -e "${y} pip2 install requests
-echo -e "${y} pip2 install mechanize
-echo -e "${y} python2 dark.py"
-
-echo
-;;
-
-#Install Ipcs
-
-2) git clone https://github.com/kancotdiq/ipcs
-echo -e "${y} Installer Ipcs..."
-echo -e "${y} cd ipcs"
-echo -e "${y} pip2 install requests"
-echo -e "${y} python2 scan.py
-
-echo
-
-;;
-
-# Nmap
-
-case $pil in
-3) pkg install nmap
-echo -e "${y} {1} Masukkan Web${endc}:"
-read web
-nmap $web
-echo
-
-;;
-
-#Install Th3inspector
-
-4) git clone https://github.com/Moham3dRiahi/Th3inspector.git
-echo -e "${y} SUDAH TERINSTALL YA NGENTOD.."
-
-
-;;
+b="\033[0;34m"
+g="\033[1;32m"
+w="\033[1;37m"
+r="\033[1;31m"
+y="\033[1;33m"
+cyan = "\033[0;36m"
+lgray = "\033[0;37m"
+dgray = "\033[1;30m"
+ir = "\033[0;101m"
+reset = "\033[0m"
 
 
 
-#Install Hammer
+def main():
+    os.system('clear')
+        print("{}  ___________________[]________\").format(r)
+        print("  /    | []    []          |       \ ")
+       print("  /     |         []    []  |        \ ")
+        print(" \     | []    []          |        /")
+       print("{} \____|____________[]_____|_______/ ").format(w)
+    print("{}  '--------------'----------{}------------------.  ").format(r,w)
+    print("{}  | {}Created  : {}CoumingX {}     | {}INDO{}N{}{}ESIA         | ").format(r,w,r,w,r,ir,reset,w)
+    print("{}  | {}Youtube  : {}Beny Galon {}| {}+62-838-1434-0681 {}|").format(r,w,w,w,lgray,w)
+    print("{}  '------------------------------------{}-------'  ").format(r,w)
+    print ("  {}[ 1 ] {}Denmark").format(r,w)
+    print ("  {}[ 2 ] {}Indonesia").format(r,w)
+    print ("  {}[ 3 ] {}Japan").format(r,w)
+    print ("  {}[ 4 ] {}Spanyol").format(r,w)
+    print ("  {}[ 5 ] {}Jamaika").format(r,w)
+    print ("  {}[ 6 ] {}Korea").format(r,w)
+    print ("  {}[ 7 ] {}Inggris").format(r,w)
+    print ("  {}[ 8 ] {}Saudi Arabia").format(r,w)
+    print ("  {}[ 9 ] {}Exit").format(r,w)
+    print ""
+    select = input("\033[1;31m[ \033[1;37mSelect@Number \033[1;31m]\033[1;37m> ")
+    filtering(select)
 
-5) git clone https://github.com/cyweb/hammer
-echo -e "${y} SUDAH TERINSTALL YA NGENTOD.."
-
-;;
 
 
-00) echo "USURNAME: CoumingX" | lolcat
-echo "CoumingX ONLY SYSTEM" | lolcat
-echo "ANONYMOUS " | lolcat
-echo "YANG BUAT TOOLS MASIH NOB" | lolcat
-figlet Th3InspecTor | lolcat
-exit
-;;
+def filtering(pilih):
+    if pilih == 1:
+        denmark()
+    elif pilih == 2:
+        indonesia()
+    elif pilih == 3:
+        japan()
+    elif pilih == 4:
+        spanyol()
+    elif pilih == 5:
+        jamaika()
+    elif pilih == 6:
+        korea()
+    elif pilih == 7:
+        inggris()
+    elif pilih == 8:
+        saudiarabia()
+    elif pilih == 9:
+        print (r+"Exiting ..."+w)
+        os.sys.exit()
+    else:
+        print (r+"Exiting ..."+w)
+        os.sys.exit()
 
-*) echo "Sorry, Your choices it's not already [T4T]"
-esac
-done
-done
+if __name__ == '__main__':
+    main()
 
